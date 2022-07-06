82499de7ce96e47375534a85499f507dac1252c7
gsierra22 <giancarlosierra2011@hotmail.com>
2022-07-06 14:05:55 -0500

    service work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptors';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import { GaugeModule } from 'angular-gauge';
 import {MatTabsModule} from '@angular/material/tabs'
 import {MatIconModule} from '@angular/material/icon'
@@ -32,7 +33,16 @@ import { HomeComponent } from './components/home/home.component'
     MatFormFieldModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpHeadersInterceptor,
+    multi:true
+  },{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpHeadersInterceptor,
+    multi:true
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
